c3f28df22c0c0325453526372d10d6318431a0db
Maryna Danyliuk <marynadanyliuk295@gmail.com>
2024-08-22 23:22:02 +0300

    added pagination

--- src/containers/Ordering/ModifiersList/ModifiersList.jsx
+++ src/containers/Ordering/ModifiersList/ModifiersList.jsx
@@ -1,10 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid, Paper, Typography } from '@mui/material';
+import { Grid, Pagination, Paper, Typography } from '@mui/material';
 import { addModifier } from '@/store/slice/productsSlice';
+import { useState } from 'react';
 
 const ModifiersList = () => {
   const dispatch = useDispatch();
   const modifiers = useSelector(state => state.modifiersReducer.modifiers);
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 12;
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const paginatedModifiers = modifiers.slice((page - 1) * itemsPerPage, page * itemsPerPage);
 
   const addClickModifier = (modifier) => {
     dispatch(addModifier(modifier))
@@ -13,7 +21,7 @@ const ModifiersList = () => {
   return (
     <div>
       <Grid container spacing={2}>
-        {modifiers.map((modifier) => (
+        {paginatedModifiers.map((modifier) => (
           <Grid item xs={12} sm={6} md={3} key={modifier.id} onClick={() => addClickModifier(modifier)} className="grid-item">
             <Paper elevation={3} style={{ padding: 16 }}>
               <Typography variant="h6">{modifier.name}</Typography>
@@ -21,6 +29,14 @@ const ModifiersList = () => {
           </Grid>
         ))}
       </Grid>
+
+      <Pagination
+        count={Math.ceil(modifiers.length / itemsPerPage)}
+        page={page}
+        onChange={handlePageChange}
+        color="primary"
+        style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}
+      />
     </div>
   )
 };
